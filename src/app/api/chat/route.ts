18a0f22d29b22af6cfaceeb147c71d0bd3f94b05
Ritesh <riteshkokam@gmail.com>
2023-12-23 13:33:35 +0530

    add updated hfinfernece

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -171,6 +171,7 @@
 //   } catch (error) {}
 // }
 
+import { getContext } from "@/lib/context";
 import { db } from "@/lib/db";
 import { chats, messages as _messages } from "@/lib/db/schema";
 import { HfInference } from "@huggingface/inference";
@@ -185,43 +186,118 @@ const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
 // IMPORTANT! Set the runtime to edge
 export const runtime = "edge";
 
+// export async function POST(req: Request) {
+//   // Extract the `messages` from the body of the request
+//   const { messages, chatId } = await req.json();
+//   const _chats = await db.select().from(chats).where(eq(chats.id, chatId));
+//   if (_chats.length != 1) {
+//     return NextResponse.json({ error: "chat not found" }, { status: 404 });
+//   }
+//   const fileKey = _chats[0].fileKey;
+//   const lastMessage = messages[messages.length - 1];
+//   // const context = await getContext(lastMessage.content, fileKey);
+//   // const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+
+//   console.log("in post");
+
+//   const response = Hf.textGenerationStream({
+//     model: "HuggingFaceH4/starchat-beta",
+//     inputs: experimental_buildStarChatBetaPrompt(messages),
+//     parameters: {
+//       max_new_tokens: 200,
+//       // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
+//       typical_p: 0.2,
+//       repetition_penalty: 1,
+//       truncate: 1000,
+//       return_full_text: false,
+//     },
+//   });
+
+//   console.log("OpenAssistant", response);
+
+//   // Convert the response into a friendly text-stream
+//   const stream = HuggingFaceStream(response, {
+//     onStart: async () => {
+//       // save user message into db
+//       await db.insert(_messages).values({
+//         chatId,
+//         content: lastMessage.content,
+//         role: "user",
+//       });
+//     },
+//     onCompletion: async (completion) => {
+//       // save ai message into db
+//       await db.insert(_messages).values({
+//         chatId,
+//         content: completion,
+//         role: "system",
+//       });
+//     },
+//   });
+
+//   // Respond with the stream
+//   return new StreamingTextResponse(stream);
+// }
+
 export async function POST(req: Request) {
-  // Extract the `messages` from the body of the request
+  // create an instance of the HfInference class with your API key
+  const hf = new HfInference(process.env.HUGGINGFACE_API_KEY);
+  
+  // get the messages and chatId from the request body
   const { messages, chatId } = await req.json();
   const _chats = await db.select().from(chats).where(eq(chats.id, chatId));
   if (_chats.length != 1) {
     return NextResponse.json({ error: "chat not found" }, { status: 404 });
   }
   const fileKey = _chats[0].fileKey;
+  
+  // get the last user message
   const lastMessage = messages[messages.length - 1];
-  // const context = await getContext(lastMessage.content, fileKey);
-  // const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+  
+  // get the context from the current open tab on edge
+  const context = await getContext(lastMessage.content, fileKey);
+  
+  // define the chat model and the context block
+  const chatModel = 'HuggingFaceH4/starchat-beta';
+  const contextBlock = `AI assistant is a brand new, powerful, human-like artificial intelligence.
+  The traits of AI include expert knowledge, helpfulness, cleverness, and articulateness.
+  AI is a well-behaved and well-mannered individual.
+  AI is always friendly, kind, and inspiring, and he is eager to provide vivid and thoughtful responses to the user.
+  AI has the sum of all knowledge in their brain, and is able to accurately answer nearly any question about any topic in conversation.
+  AI assistant is a big fan of Pinecone and Vercel.
+  START CONTEXT BLOCK
+  ${context}
+  END OF CONTEXT BLOCK
+  AI assistant will take into account any CONTEXT BLOCK that is provided in a conversation.
+  If the context does not provide the answer to question, the AI assistant will say, "I'm sorry, but I don't know the answer to that question".
+  AI assistant will not apologize for previous responses, but instead will indicated new information was gained.
+  AI assistant will not invent anything that is not drawn directly from the context.`;
 
-  console.log("in post");
+  // append the context block to the messages array
+  messages.push({
+    role: 'system',
+    content: contextBlock,
+  });
 
-  const response = Hf.textGenerationStream({
-    model: "HuggingFaceH4/starchat-beta",
+  // create a text generation stream from the Huggingface Inference API
+  const response = await hf.textGenerationStream({
+    model: chatModel,
     inputs: experimental_buildStarChatBetaPrompt(messages),
     parameters: {
       max_new_tokens: 200,
-      // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
-      typical_p: 0.2,
+      temperature: 0.5,
       repetition_penalty: 1,
-      truncate: 1000,
       return_full_text: false,
     },
   });
 
-  console.log("OpenAssistant", response);
-
-  // Convert the response into a friendly text-stream
   const stream = HuggingFaceStream(response, {
     onStart: async () => {
       // save user message into db
       await db.insert(_messages).values({
         chatId,
         content: lastMessage.content,
-        role: "user",
+        role: 'user',
       });
     },
     onCompletion: async (completion) => {
@@ -229,11 +305,11 @@ export async function POST(req: Request) {
       await db.insert(_messages).values({
         chatId,
         content: completion,
-        role: "system",
+        role: 'system',
       });
-    },
-  });
+    }
+  })
 
-  // Respond with the stream
+  // return a streaming text response with the stream and the callbacks
   return new StreamingTextResponse(stream);
 }
