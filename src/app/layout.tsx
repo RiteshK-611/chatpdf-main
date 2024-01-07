456ec3b11d93d39835020a9b6728f359bef1d530
Ritesh <riteshkokam@gmail.com>
2024-01-07 22:25:16 +0530

    rename the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ChatPDF YT",
+  title: "ChatPDF",
 };
 
 export default function RootLayout({
