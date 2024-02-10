59e081d4c931839851e15d9c1ccd6c2f6cc0e0e3
Ritesh Kokam <61982298+RiteshK-611@users.noreply.github.com>
2024-02-10 23:00:02 +0530

    Revert "add delete chat funcitonality"

--- src/components/ChatSideBar.tsx
+++ src/components/ChatSideBar.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { DrizzleChat } from "@/lib/db/schema";
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "./ui/button";
-import { MessageCircle, PlusCircle, Trash } from "lucide-react";
+import { MessageCircle, PlusCircle } from "lucide-react";
 import { cn } from "@/lib/utils";
 import axios from "axios";
-import { toast } from "react-hot-toast";
+import SubscriptionButton from "./SubscriptionButton";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 
 type Props = {
   chats: DrizzleChat[];
@@ -14,29 +15,8 @@ type Props = {
   isPro: boolean;
 };
 
-const ChatSideBar = ({ chats: initialChats, chatId, isPro }: Props) => {
-  const [chats, setChats] = useState<DrizzleChat[]>(initialChats);
-  const [loading, setLoading] = useState(false);
-
-  const handleDelete = async (id: number) => {
-    // Display a toast with a spinner
-    const toastId = toast.loading("Deleting chat...");
-
-    try {
-      const response = await axios.post("/api/delete-chat", { chatId: id });
-      if (response.status === 200) {
-        // If the deletion was successful, update the toast and the state
-        toast.success("Chat deleted successfully", { id: toastId });
-        setChats(response.data.chats);
-      } else {
-        // If there was an error, update the toast
-        toast.error("Failed to delete chat", { id: toastId });
-      }
-    } catch (error) {
-      console.error("Failed to delete chat:", error);
-      toast.error("Failed to delete chat", { id: toastId });
-    }
-  };
+const ChatSideBar = ({ chats, chatId, isPro }: Props) => {
+  const [loading, setLoading] = React.useState(false);
 
   return (
     <div className="w-full max-h-screen soff p-4 text-gray-200 bg-gray-900">
@@ -53,24 +33,17 @@ const ChatSideBar = ({ chats: initialChats, chatId, isPro }: Props) => {
         {chats.map((chat) => (
           <Link key={chat.id} href={`/chat/${chat.id}`}>
             <div
-              className={cn(
-                "rounded-lg p-3 text-slate-300 flex items-center group",
-                {
-                  "bg-blue-600 text-white": chat.id === chatId,
-                  "hover:text-white": chat.id !== chatId,
-                }
-              )}>
+              className={cn("rounded-lg p-3 text-slate-300 flex items-center", {
+                "bg-blue-600 text-white": chat.id === chatId,
+                "hover:text-white": chat.id !== chatId,
+              })}>
               <MessageCircle className="mr-2" />
               <p className="w-full overflow-hidden text-sm truncate whitespace-nowrap text-ellipsis">
                 {chat.pdfName}
               </p>
-              <Trash
-                className="ml-2 collapse group-hover:visible"
-                onClick={() => handleDelete(chat.id)}
-              />
             </div>
           </Link>
-        ))}
+        ))} 
       </div>
     </div>
   );
