ed9502662d1add6cb54605ca0917aa239e0b5d4f
Ritesh <riteshkokam@gmail.com>
2025-05-20 22:57:45 +0530

    chore: update Stripe dependency to version 18.1.1 and adjust API version in integration

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY as string, {
-  apiVersion: "2023-08-16",
+  apiVersion: "2025-04-30.basil",
   typescript: true,
 });
