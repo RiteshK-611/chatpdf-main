499d84ff89ddbd65ca251ee3c034c0f0e14d7c1c
Ritesh <riteshkokam@gmail.com>
2025-05-22 14:27:17 +0530

    Revert "chore: update Stripe dependency to version 18.1.1 and adjust API version in integration"
    This reverts commit ed9502662d1add6cb54605ca0917aa239e0b5d4f.

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
 
   // new subscription created
   if (event.type === "checkout.session.completed") {
-    const subscription = await stripe.subscriptionItems.retrieve(
+    const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     );
     if (!session?.metadata?.userId) {
@@ -33,20 +33,20 @@ export async function POST(req: Request) {
     await db.insert(userSubscriptions).values({
       userId: session.metadata.userId,
       stripeSubscriptionId: subscription.id,
-      stripeCustomerId: session.customer as string,
-      stripePriceId: subscription.price.id,
+      stripeCustomerId: subscription.customer as string,
+      stripePriceId: subscription.items.data[0].price.id,
       stripeCurrentPeriodEnd: new Date(subscription.current_period_end * 1000),
     });
   }
 
   if (event.type === "invoice.payment_succeeded") {
-    const subscription = await stripe.subscriptionItems.retrieve(
+    const subscription = await stripe.subscriptions.retrieve(
       session.subscription as string
     );
     await db
       .update(userSubscriptions)
       .set({
-        stripePriceId: subscription.price.id,
+        stripePriceId: subscription.items.data[0].price.id,
         stripeCurrentPeriodEnd: new Date(
           subscription.current_period_end * 1000
         ),
